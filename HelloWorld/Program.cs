736968c9718d341f0d53678752cd6ec04227d5fd
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-05 21:52:17 -0300

    learning-syntax

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,19 @@ namespace NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Olá, Qual é a sua idade?:");
+            int idade = int.Parse(Console.ReadLine());
+            if(idade < 18 && idade > 0){
+                Console.WriteLine("Você é uma criança");
+            }
+            else if(idade >= 18){
+                Console.WriteLine("Você é um adulto");
+            }
+            else{
+                Console.WriteLine("Você é um idoso");
+            }
+            Console.WriteLine("Hello World");
+
         }
     }
 }
