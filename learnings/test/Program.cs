c59e2f9e3f36b318661c6c3fce27dcfa917b98fa
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-06 01:19:34 -0300

    ill-fix-after

---
+++ learnings/test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+        Console.WriteLine("Qual a nota do aluno?");
+           int nota = int.Parse(Console.ReadLine());
+
+            bool alunoA = nota >= 80;
+            bool alunoB = nota >= 70;
+            bool alunoC = nota >= 60;
+            bool alunoD = nota >= 50;
+            bool alunoE = nota <= 49;
+
+            switch(true){
+                case alunoA: 
+                    Console.WriteLine("Parabens, nota A!");
+                break;
+                
+                case alunoB: 
+                    Console.WriteLine("Você tirou nota B");
+                break;
+                    
+                case alunoC:
+                    Console.WriteLine("Você tirou nota C");
+                break;
+
+                case alunoD:
+                    Console.WriteLine("Voce tirou nota D");
+                break;
+
+                case alunoE:
+                    Console.WriteLine("Você tirou nota E");
+                break;
+                default:
+                break;
+            }
+
+
+        }
+    }
+}
