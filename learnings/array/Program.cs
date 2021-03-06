9391a63c7342583ffbd5bf955aab0af019292304
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-05 23:46:41 -0300

    learning-arrays

---
+++ learnings/array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = returnName();
+            string[] nomes = {name,"alessandra"};
+
+            Console.WriteLine(nomes[0]);
+            Console.ReadLine();
+        }
+        static string returnName(){
+            return "jhon";
+        }
+    }
+}
