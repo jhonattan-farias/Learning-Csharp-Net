8ceb52633f430774397e2e41ee6a3f85e0173210
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-06 07:38:45 -0300

    cars-method

---
+++ learnings/criandoCarro/Carro.cs
@@ -0,0 +1,16 @@
+namespace criandoCarro
+{
+    public class Carro
+    {
+        public string model;
+        public int year;
+        public float price;
+
+            public Carro(string aModel,int aYear, float aPrice)
+            {
+                model = aModel;
+                year = aYear;
+                price = aPrice;
+            }
+    }
+}
---
+++ learnings/criandoCarro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace criandoCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o Modelo do seu carro?");
+            string model = Console.ReadLine();
+           
+            Console.WriteLine("Qual o ano é o ano do seu carro?");
+            int year = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Qual o preço do seu Carro?");
+            float price = float.Parse(Console.ReadLine());
+
+            Carro carro1 = new Carro(model,year,price);
+            
+            Console.WriteLine("O modelo do seu carro é " + carro1.model);
+            Console.WriteLine("O ano é " + carro1.year);
+            Console.WriteLine("E o preço do carro é " + carro1.price + " Certo?");
+            Console.ReadLine();
+        }
+    }
+}
